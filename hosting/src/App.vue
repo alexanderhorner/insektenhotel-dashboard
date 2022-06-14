a6ab2ec5eb958b713d084f117cc7de4aff775f4d
Alexander Horner <mail@alexanderhorner.com>
2022-06-14 02:04:11 +0200

    Basic visualizatin with charts and tables.

--- hosting/src/App.vue
+++ hosting/src/App.vue
@@ -21,54 +21,119 @@ const database = getDatabase();
 if (location.hostname === "localhost") {
   // Point to the RTDB emulator running on localhost.
   connectDatabaseEmulator(database, "localhost", 9000);
-} 
+}
 
-const currentDataDataObj = ref(null)
-const historyDataObj = ref(null)
+const currentData = ref({})
+const history = ref(new Array)
+const datafields = ref(new Array())
+const historyGroupedByFieled = ref(new Array()) // convert to obj
+
+const temperature_1 = ref(new Array)
 
 const currentDataDBRef = dbref(database, 'currentData/');
 onValue(currentDataDBRef, (snapshot) => {
-  const val = snapshot.val()
-  currentDataDataObj.value = val
+  const val = snapshot.val();
+  currentData.value = val
 });
 
 const historyDBRef = dbref(database, 'history/');
 onValue(historyDBRef, (snapshot) => {
-  const val = snapshot.val()  
-  historyDataObj.value = val
-});
+  const rawDatabaseEntry: Object = snapshot.val();
+
+  let allEntriesRaw = Object.values(rawDatabaseEntry).reverse()
+
+  // Gather all datafields
+  datafields.value = Array.from(new Set( // make unique by using Set
+    allEntriesRaw.flatMap(entry => { // get all keys in array
+      return Object.keys(entry)
+    }).sort()
+  ))
+  
+  // Create template Entry Object with all datafields
+  const templateEntry = datafields.value.reduce((previousVal: Object, currentVal) => {
+    // @ts-ignore
+    previousVal[currentVal] = null
+    return previousVal
+  }, {})
+
+  // Equalize all objects to the templayte Entry Object
+  const allEntries = history.value = allEntriesRaw.map(entry => {
+    return {
+      ...templateEntry,
+      ...entry
+    }
+  })
+
+
+  historyGroupedByFieled.value = (() => {
+    return Object.entries(templateEntry).map(([datafield, item]) => {
+      return allEntries.map(entry => entry[datafield])
+    })
+  })()
+
+  // console.log(historyGroupedByFieled)
+
+})
 
 </script>
 
 <template>
-  <!-- <table>
-    <tbody>
-        <tr v-for="(item, key) in currentDataDataObj">
-            <td>{{ key }}: </td>
-            <td>{{ item }}</td>
-        </tr>
-    </tbody>
-  </table> -->
-  <!-- <h1 v-for="dataPoint in historyDataObj">{{ dataPoint }}</h1> -->
+  <!-- <widget-small :data="temperature_1"></widget-small> -->
+  <div v-for="(item, key) in datafields">
+    <h1>{{ item }}:</h1>
+    <widget-small :data="historyGroupedByFieled[key]"></widget-small>
+  </div>
+  
 
-  <widget-small></widget-small>
 
-  <table>
-    <thead>
-        <tr>
-            <th v-for="(item, key) in currentDataDataObj">{{ key }}</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr v-for="dataPoint in historyDataObj">
-            <td v-for="item in dataPoint">{{ item }}</td>
-        </tr>
-    </tbody>
-  </table>
+  <h1>Gerade eben: </h1>
+
+  <div class="table-wrapper">
+    <table class="table">
+      <thead>
+          <tr>
+              <th v-for="(item, key) in currentData">{{ key }}</th>
+          </tr>
+      </thead>
+      <tbody>
+          <tr>
+              <td v-for="(item, key) in currentData">{{ item }}</td>
+          </tr>
+      </tbody>
+    </table>
+  </div>
+
+  <h1>Verlauf:</h1>
+  <div class="table-wrapper">
+    <table class="table">
+      <thead>
+          <tr>
+              <th v-for="key in datafields">{{ key }}</th>
+          </tr>
+      </thead>
+      <tbody>
+          <tr v-for="dataPoint in history">
+              <td v-for="item in dataPoint">{{ item }}</td>
+          </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
-<style lang="scss">
-table {
+<style lang="scss" scoped>
+.ml-50px, h1 {
+  margin-left: 50px;
+}
+.table-wrapper {
+  display: block;
+  width: 100%;
+  overflow: scroll;
+}
+.table {
+  border-collapse: collapse;
+  display: inline-table;
+  margin: 0 50px;
+
   td, th {
     padding: 0.5em 2em;
     border: 1px solid hsl(0, 0%, 30%)
--- hosting/src/components/WidgetSmall.vue
+++ hosting/src/components/WidgetSmall.vue
@@ -1,15 +1,69 @@
 <script setup lang="ts">
+// import { Chart, LineController, CategoryScale, LinearScale, PointElement, LineElement } from 'chart.js';
+// import { LineChart } from 'vue-chart-3'
+// Chart.register(LineController, CategoryScale, LinearScale, PointElement, LineElement)
+
+import { LineChart } from 'vue-chart-3';
+import { Chart, registerables } from "chart.js";
+import { computed, ref, toRef, watch } from 'vue';
+Chart.register(...registerables);
+
+const props = defineProps({
+  data: {
+    type: Array,
+    required: true
+  }
+})
+const dataArray = toRef(props, 'data')
+
+const labels = computed(() => {
+  return dataArray.value.map(item => {
+    return "Label"
+  })
+})
+
+const data = computed(() => {
+  return {
+    labels: labels.value,
+    datasets: [{
+      data: dataArray.value.reverse(),
+      tension: 0.3,
+      pointRadius: 0,
+      borderColor: 'rgb(75, 192, 192)',
+    }]
+  }
+})
+
+const options = {
+  layout: {
+      padding: 20
+  },
+  scales: {
+    x: {
+      display: false
+    },
+  //   y: {
+  //     display: false,
+  //   }
+  },
+  plugins: {
+    legend: {
+      display: false
+    }
+  },
+  events: []
+}
+
+const asAny = (foo: any) => foo as any;
 </script>
 
 <template>
-    <div class="root">
-
-    </div>
+  <div class="root">
+    <LineChart class="chart" :chartData="asAny(data)" :options="options"/>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-.root {
-    
-}
+
 
 </style>
